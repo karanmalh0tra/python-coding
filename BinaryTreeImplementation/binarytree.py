31efcb9d734aeb12298c3af41c346281b706d56e
Karan Malhotra <m9.karan@gmail.com>
2020-04-28 12:13:44 +0530

    implement depth

--- BinaryTreeImplementation/binarytree.py
+++ BinaryTreeImplementation/binarytree.py
@@ -86,6 +86,24 @@ def numLeafNodes(root):
     numLeafRight = numLeafNodes(root.right)
     return numLeafLeft + numLeafRight
 
+def printDepthK(root,k):
+    if root == None:
+        return
+    if k == 0:
+        print(root.data)
+        return
+    printDepthK(root.left,k-1)
+    printDepthK(root.right,k-1)
+
+def printDepthKV2(root,k,d=0):
+    if root == None:
+        return
+    if k == d:
+        print(root.data)
+        return
+    printDepthKV2(root.left,k,d+1)
+    printDepthKV2(root.right,k,d+1)
+
 root = treeInput()
 printTreeDetailed(root)
 print(numNodes(root))
