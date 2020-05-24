17078d45704a13c538269bb5211278ad29c49cb6
Karan Malhotra <m9.karan@gmail.com>
2020-05-25 00:25:01 +0530

    LIS DP approach

--- DynamicProgramming/LIS.py
+++ DynamicProgramming/LIS.py
@@ -10,7 +10,7 @@ def LISRecursive(li,i,n):
     overall_max = max(including_max,excluding_max)
     return including_max, overall_max
 
-def LISMemoization(li,i,n,dp):
+def LISMemoization(li,i,n,dp): #Memoization
     if i == n:
         return 0,0
     including_max = 1
@@ -32,8 +32,23 @@ def LISMemoization(li,i,n,dp):
     overall_max = max(including_max,excluding_max)
     return including_max, overall_max
 
+def LISIterative(li,n): #Dynamic Problem
+    dp = [[0 for j in range(2)] for i in range(n+1)]
+    
+    for i in range(n-1,-1,-1):
+        including_max = 1
+        for j in range(i+1,n):
+            if li[j] > li[i]:
+                including_max = max(including_max, 1+dp[j][0])
+        dp[i][0] = including_max
+        excluding_max = dp[i+1][1]
+        overall_max = max(including_max, excluding_max)
+        dp[i][1] = overall_max
+    return dp[0][1]
+
+
 n = int(input())
 li = [int(ele) for ele in input().split()]
-dp = [-1 for i in range(n+1)]
-ans = LISMemoization(li,0,n,dp)[1]
+#dp = [-1 for i in range(n+1)]
+ans = LISIterative(li,n)
 print(ans)
