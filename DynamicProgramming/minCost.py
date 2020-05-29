29e6f7ba13a1f8bcda163adaab05cc60d42f997a
Karan Malhotra <m9.karan@gmail.com>
2020-05-29 23:28:59 +0530

    top down approach DP

--- DynamicProgramming/minCost.py
+++ DynamicProgramming/minCost.py
@@ -39,7 +39,7 @@ def minCostMemoization(cost,i,j,m,n,dp):
     my_cost = cost[i][j] + min(ans1,ans2,ans3)
     return my_cost
 
-def minCostIterative(cost,m,n): #Dynamic Programming
+def minCostIterative(cost,m,n): #Dynamic Programming Bottom Up Approach
     dp = [[sys.maxsize for j in range(n+1)] for i in range(m+1)]
     for i in range(m-1,-1,-1):
         for j in range(n-1,-1,-1):
@@ -52,13 +52,26 @@ def minCostIterative(cost,m,n): #Dynamic Programming
                 dp[i][j] = cost[i][j]+ min(ans1,ans2,ans3)
     return dp[0][0]
 
+def minCostIterativeTD(cost,m,n): #Dynamic Programming Bottom Up Approach
+    dp = [[sys.maxsize for j in range(n+1)] for i in range(m+1)]
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if i == 1 and j == 1:
+                dp[i][j] = cost[0][0]
+            else:
+                ans1 = dp[i][j-1]
+                ans2 = dp[i-1][j]
+                ans3 = dp[i-1][j-1]
+                dp[i][j] = cost[i-1][j-1]+ min(ans1,ans2,ans3)
+    return dp[m][n]
+
 cost = [[1,5,11],[8,13,12],[2,3,7],[15,16,18]]
 m = 4
 n = 3
 #dp = [[sys.maxsize for j in range(n+1)] for i in range(m+1)]
 #ans = minCostRecursive(cost,0,0,4,3)
 #ans = minCostMemoization(cost,0,0,m,n,dp)
-ans = minCostIterative(cost,m,n)
+ans = minCostIterativeTD(cost,m,n)
 print(ans)
 
 #   1   5   11
